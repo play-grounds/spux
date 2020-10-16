ab1096e8ad6b64a88762b666d54f49fb459174f0
Melvin Carvalho <melvincarvalho@gmail.com>
2020-10-16 21:41:20 +0200

    fix change precache urls

--- serviceworker.js
+++ serviceworker.js
@@ -18,15 +18,7 @@ const PRECACHE = 'precache-v1'
 const RUNTIME = 'runtime'
 
 // A list of local resources we always want to be cached.
-const PRECACHE_URLS = [
-  'index.html',
-  './', // Alias for index.html
-  'styles.css',
-  '../../styles/main.css',
-  'demo.js',
-  'pwa.html',
-  'spux/pwa.html'
-]
+const PRECACHE_URLS = ['pwa.html']
 
 // The install handler takes care of precaching the resources we always need.
 self.addEventListener('install', event => {
